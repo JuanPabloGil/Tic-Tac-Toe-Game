ee69eeb51d1bf75dac85ed699388fe6e7609d957
juanpablogil <jpablomgil@gmail.com>
2019-10-15 12:02:52 -0500

    Adding hello world to main

--- bin/main.rb
+++ bin/main.rb
@@ -1,14 +1,2 @@
 #!/usr/bin/env ruby
- def players_choise
-
-    if player_one=='X'
-        player_two='O'
-    elsif player_two=='X'
-        player_one="O"
- end
-
-puts "player_one what do you choose: X or O"
-player_one=gets.chomp
-
-
-
+puts "Hello World!"
